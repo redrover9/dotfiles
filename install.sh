ba276856fde1f1c7cc1aa5380a9ae60e0dc810b7
redrover9 <grace.thompson97@gmail.com>
2021-09-17 12:33:41 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+zypper in i3
+zypper in zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
