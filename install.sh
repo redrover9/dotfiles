ec3c055412c41143f1fb4b4d36661203b8e5787f
redrover9 <grace.thompson97@gmail.com>
2021-09-18 11:27:25 -0700

    change to Xmonad

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-zypper in i3
+zypper in xmonad
 zypper in zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
