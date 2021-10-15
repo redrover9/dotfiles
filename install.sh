35784e97546799d3510ebf0d295b2bb8e27a7b05
redrover9 <grace.thompson97@gmail.com>
2021-10-14 19:51:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-zypper in xmonad
 zypper in zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+echo "colorscheme gruvbox" >> .vimrc
